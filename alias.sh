ef1ac09549bb2d614b61af616fc528c49862217c
lgg <fed777os@gmail.com>
2020-06-26 15:30:25 +0300

    update script

--- alias.sh
+++ alias.sh
@@ -1,36 +1,58 @@
 alias nci='neuro_core_init'
 
 neuro_core_init (){
-    readmeUrl="https://raw.githubusercontent.com/neuro-core/templates/master/readme-template.md"
-    reportUrl="https://raw.githubusercontent.com/neuro-core/templates/master/report-template.md"
+    githubUrl="https://raw.githubusercontent.com/neuro-core/templates/master/"
+    readmeFileName="readme-template.md"
+    reportFileName="report-template.md"
+
+    readmeUrl="$githubUrl""$readmeFileName"
+    reportUrl="$githubUrl""$reportFileName"
 
     readme (){
-        echo "creating readme template"
-        wget ${readmeUrl}
+        echo "Creating readme template"
+        wget -O ${1} ${readmeUrl}
     }
 
     report (){
-        echo "creating report template"
-        wget ${reportUrl}
+        echo "Creating report template"
+        wget -O ${1} ${reportUrl}
     }
+
+    usage (){
+        echo "Usage: $0 {d|readme|r|report}"
+    }
+
+    # we have no file argument
+    if [[ -z "$1" ]]; then
+        echo "No task"
+        usage
+        exit 1
+    fi
+
+    # we have new output filename
+    if [[ -z "$2" ]]; then
+        echo "Use default template name"
+    else
+        readmeFileName=$2
+        reportFileName=$2
+    fi
 
     case "$1" in
             d)
-                readme
+                readme ${readmeFileName}
                 ;;
-
             readme)
-                readme
+                readme ${readmeFileName}
                 ;;
-
             r)
-                report
+                report ${reportFileName}
                 ;;
             report)
-                report
+                report ${reportFileName}
                 ;;
             *)
-                echo $"Usage: $0 {d|readme|r|report}"
+                echo "Unknown task"
+                usage
                 exit 1
     esac
 }
